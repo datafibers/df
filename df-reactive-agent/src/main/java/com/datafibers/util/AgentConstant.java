e5312be273dcf4afb26c7541ef3fd30f43ace622
Will Du <Will.Du@td.com>
2016-07-14 10:52:36 -0400

    fix file fiter for exclude files

--- df-reactive-agent/src/main/java/com/datafibers/util/AgentConstant.java
+++ df-reactive-agent/src/main/java/com/datafibers/util/AgentConstant.java
@@ -5,20 +5,33 @@ package com.datafibers.util;
  */
 public class AgentConstant {
 
+    //Server settings - need to move to config/param files later
+    public static String SERVER_ADDR = "localhost";
+    public static int SERVER_PORT = 8998;
+    public static int RES_SUCCESS = 202;
+
+    //Internal constant
     public static String FILE_NAME = null;
     public static String FILE_TOPIC = null;
     public static String META_TOPIC = "META_DATA"; //use default value for marking handshake
     public static String TRANS_MODE = null;
-    public static String SERVER_ADDR = "localhost";
-    public static int SERVER_PORT = 8998;
-    public static int RES_SUCCESS = 202;
-    public static String IGNORE_FILES_PREFIX = "_";
-    public static String PROCESSING_FILES_POSTFIX = ".processing";
-    public static String PROCESSED_FILES_POSTFIX = ".processed";
-    public static int COUNTER = 0;
+
+    //File processing folder setting - not used right now since we pick up directly from file folder parsed from CML
     public static String PROCESSING_FOLDER = "/home/vagrant/processing/";
     public static String PROCESSED_FOLDER = "/home/vagrant/processed/";
     public static String LANDING_FOLDER = "/home/vagrant/landing/";
-    public static String FILE_FILTER_REGX = "([^\\s]+(\\.(?i)(txt|csv|json|xml))$)";
+
+    //File extensions internal used or ignored
+    public static String IGNORE_FILES_POSTFIX = ".ignore";
+    public static String PROCESSING_FILES_POSTFIX = ".processing";
+    public static String PROCESSED_FILES_POSTFIX = ".processed";
+    public static String FILE_FILTER_REGX = ".\\.(?!(js|exe" +
+            (IGNORE_FILES_POSTFIX + PROCESSING_FILES_POSTFIX + PROCESSED_FILES_POSTFIX).replace('.','|') +
+            ")$)([^.]+$)";
+
+    //Vertx and other control parameters
+    public static int COUNTER = 0;
     public static int FILE_WATCHER_PERIODIC = 1000; //1000 is 1 second
+
+
 }
